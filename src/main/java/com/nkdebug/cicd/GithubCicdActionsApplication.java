33e0e76f772f2ac360757e92556322204a460335
nitesh009 <niteshgnit3@gmail.com>
2025-09-15 21:32:20 +0530

    first commit

--- src/main/java/com/nkdebug/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/nkdebug/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome to github actions !";
+		return "Welcome to github actions V2 !";
 	}
 
 //	echo "# github-actions-example" >> README.md
